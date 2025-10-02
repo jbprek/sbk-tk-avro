318d7728a7500035f82b8ab586a8523d57cc8917
jbprek <jbprek@gmail.com>
2025-10-02 11:03:31 +0100

    Schema changes

--- docker/mysql/init/01-births-schema.sql
+++ docker/mysql/init/01-births-schema.sql
@@ -6,11 +6,10 @@ GRANT ALL PRIVILEGES ON births.* TO 'births'@'%';
 CREATE TABLE IF NOT EXISTS births.births
 (
     reg_id BIGINT NOT NULL PRIMARY KEY,
-    name VARCHAR(100),
-    dob DATE,
-    town VARCHAR(50),
-    tm TIMESTAMP,
-    weight  DECIMAL(3,1),
-    UNIQUE KEY unique_name_dob (name, dob, town)
+    name VARCHAR(100) NOT NULL,
+    dob DATE NOT NULL,
+    town VARCHAR(50) NOT NULL,
+    reg_tm TIMESTAMP,
+    weight  DECIMAL(3,1)
 
 ) ENGINE = InnoDB;
