b0fe7aaca12d81f140e30d952c83cf1ad7401771
jbprek <jbprek@gmail.com>
2025-10-03 07:22:05 +0100

    WIP

--- sbk-scloud-tx-consumer-avro/src/main/java/foo/kafka/tx/consumer/SbkCloudTxConsumerApplication.java
+++ sbk-scloud-tx-consumer-avro/src/main/java/foo/kafka/tx/consumer/SbkCloudTxConsumerApplication.java
@@ -1,15 +1,8 @@
 package foo.kafka.tx.consumer;
 
-import foo.avro.birth.BirthEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.function.Consumer;
-
-import foo.kafka.tx.consumer.service.Processor;
-import org.springframework.messaging.Message;
 
 @SpringBootApplication
 @Slf4j
